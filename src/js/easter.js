5c72f023a891dcb58ccae5c969cac4dc9e625c46
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2020-03-25 09:08:14 -0600

    Update for stay-at-home

--- src/js/easter.js
+++ src/js/easter.js
@@ -1,56 +1,55 @@
-$(document).ready(function () {
-  var easterHeight = $('#easter-sunday').height(),
-    fridayHeight = $('#good-friday').height(),
-    eggHuntHeight = $('#egg-hunt').height(),
+$(document).ready(function() {
+  var easterHeight = $("#easter-sunday").height(),
+    fridayHeight = $("#good-friday").height(),
+    eggHuntHeight = $("#egg-hunt").height(),
     stickEaster = easterHeight - $(window).height(),
-    stickFriday = easterHeight + fridayHeight - $(window).height();
-    stickEgg = easterHeight + fridayHeight + eggHuntHeight - $(window).height();
+    stickEgg = easterHeight + eggHuntHeight - $(window).height();
 
-    $('#easter-sunday').css('z-index', '1');
+  $("#easter-sunday").css("z-index", "1");
 
-  window.addEventListener('resize', function () {
-      easterHeight = $('#easter-sunday').height(),
-      fridayHeight = $('#good-friday').height(),
-      eggHuntHeight = $('#egg-hunt').height(),
-      stickEaster = easterHeight - $(window).height(),
-      stickFriday = easterHeight + fridayHeight - $(window).height();
-      stickEgg = easterHeight + fridayHeight + eggHuntHeight - $(window).height();
+  window.addEventListener("resize", function() {
+    (easterHeight = $("#easter-sunday").height()),
+      (fridayHeight = $("#good-friday").height()),
+      (eggHuntHeight = $("#egg-hunt").height()),
+      (stickEaster = easterHeight - $(window).height()),
+      (stickEgg = easterHeight + eggHuntHeight - $(window).height());
   });
 
   console.log(stickEaster);
-  console.log(stickFriday);
 
+  // $("#good-friday").css("top", easterHeight + "px");
+  $("#egg-hunt").css("top", easterHeight + "px");
+  // $("#church-info").css("top", easterHeight + eggHuntHeight + "px");
 
-  $('#good-friday').css('top', easterHeight + 'px');
-  $('#egg-hunt').css('top', easterHeight + fridayHeight + 'px');
-  $('#church-info').css('top', easterHeight + fridayHeight + eggHuntHeight + 'px');
-
-  window.onscroll = function () {
-    makeSticky()
-    console.log((window).pageYOffset);
+  window.onscroll = function() {
+    makeSticky();
+    console.log(window.pageYOffset);
   };
 
   function makeSticky() {
     if (window.pageYOffset > stickEaster) {
-      $('#easter-sunday').addClass('sticky');
+      $("#easter-sunday").addClass("sticky");
     } else {
-      $('#easter-sunday').removeClass('sticky');
+      $("#easter-sunday").removeClass("sticky");
     }
 
-    if (window.pageYOffset > stickFriday) {
-      $('#good-friday').css('top', 'auto').addClass('sticky');
-    } else if (window.pageYOffset < stickFriday && window.pageYOffset > stickEaster) {
-      $('#good-friday').css('top', easterHeight + 'px').removeClass('sticky');
-    }
+    // if (window.pageYOffset > stickFriday) {
+    //   $('#good-friday').css('top', 'auto').addClass('sticky');
+    // } else if (window.pageYOffset < stickFriday && window.pageYOffset > stickEaster) {
+    //   $('#good-friday').css('top', easterHeight + 'px').removeClass('sticky');
+    // }
 
-    if (window.pageYOffset > stickEgg) {
-      $('#egg-hunt').css('top', 'auto').addClass('sticky');
-    } else if (window.pageYOffset < stickEgg && window.pageYOffset > stickFriday) {
-      $('#egg-hunt').css('top', easterHeight + fridayHeight + 'px').removeClass('sticky');
-    }
-
-
-
+    // if (window.pageYOffset > stickEgg) {
+    //   $("#egg-hunt")
+    //     .css("top", "auto")
+    //     .addClass("sticky");
+    // } else if (
+    //   window.pageYOffset < stickEgg &&
+    //   window.pageYOffset > stickEaster
+    // ) {
+    //   $("#egg-hunt")
+    //     .css("top", easterHeight + "px")
+    //     .removeClass("sticky");
+    // }
   }
-
 });
